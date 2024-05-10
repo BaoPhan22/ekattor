ad0bc71a7a759b7425d3dc5d955cd15533cf1fef
BaoPhan22 <93857238+BaoPhan22@users.noreply.github.com>
2024-05-10 13:04:30 +0700

    init

---
+++ application/logs/log-2024-05-10.php
@@ -0,0 +1,68 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+ERROR - 2024-05-10 05:24:54 --> 404 Page Not Found: Assets/js
+ERROR - 2024-05-10 05:24:54 --> 404 Page Not Found: Assets/js
+ERROR - 2024-05-10 05:24:54 --> 404 Page Not Found: Assets/js
+ERROR - 2024-05-10 05:24:54 --> 404 Page Not Found: Assets/js
+ERROR - 2024-05-10 05:24:54 --> 404 Page Not Found: Assets/js
+ERROR - 2024-05-10 05:24:54 --> 404 Page Not Found: Assets/js
+ERROR - 2024-05-10 05:24:54 --> 404 Page Not Found: Assets/js
+ERROR - 2024-05-10 05:24:54 --> 404 Page Not Found: Assets/js
+ERROR - 2024-05-10 05:24:54 --> 404 Page Not Found: Uploads/system
+ERROR - 2024-05-10 05:24:57 --> 404 Page Not Found: Assets/js
+ERROR - 2024-05-10 05:24:57 --> 404 Page Not Found: Assets/js
+ERROR - 2024-05-10 05:24:57 --> 404 Page Not Found: Assets/js
+ERROR - 2024-05-10 05:24:57 --> 404 Page Not Found: Assets/js
+ERROR - 2024-05-10 05:24:57 --> 404 Page Not Found: Assets/js
+ERROR - 2024-05-10 05:24:57 --> 404 Page Not Found: Assets/js
+ERROR - 2024-05-10 05:24:57 --> 404 Page Not Found: Assets/js
+ERROR - 2024-05-10 05:24:57 --> 404 Page Not Found: Assets/js
+ERROR - 2024-05-10 05:24:58 --> 404 Page Not Found: Assets/js
+ERROR - 2024-05-10 05:24:58 --> 404 Page Not Found: Assets/js
+ERROR - 2024-05-10 05:24:58 --> 404 Page Not Found: Assets/js
+ERROR - 2024-05-10 05:24:58 --> 404 Page Not Found: Assets/js
+ERROR - 2024-05-10 05:24:58 --> 404 Page Not Found: Assets/js
+ERROR - 2024-05-10 05:24:58 --> 404 Page Not Found: Assets/js
+ERROR - 2024-05-10 05:24:58 --> 404 Page Not Found: Assets/js
+ERROR - 2024-05-10 05:24:58 --> 404 Page Not Found: Assets/js
+ERROR - 2024-05-10 05:25:15 --> Severity: Warning --> mysqli_connect(): (HY000/1049): Unknown database 'ekattor' C:\xampp74\htdocs\ekattor\application\controllers\Install.php 124
+ERROR - 2024-05-10 05:25:15 --> Severity: Warning --> mysqli_close() expects parameter 1 to be mysqli, bool given C:\xampp74\htdocs\ekattor\application\controllers\Install.php 126
+ERROR - 2024-05-10 05:25:15 --> 404 Page Not Found: Assets/js
+ERROR - 2024-05-10 05:25:15 --> 404 Page Not Found: Assets/js
+ERROR - 2024-05-10 05:25:15 --> 404 Page Not Found: Assets/js
+ERROR - 2024-05-10 05:25:15 --> 404 Page Not Found: Assets/js
+ERROR - 2024-05-10 05:25:15 --> 404 Page Not Found: Assets/js
+ERROR - 2024-05-10 05:25:15 --> 404 Page Not Found: Assets/js
+ERROR - 2024-05-10 05:25:15 --> 404 Page Not Found: Assets/js
+ERROR - 2024-05-10 05:25:15 --> 404 Page Not Found: Assets/js
+ERROR - 2024-05-10 05:26:05 --> 404 Page Not Found: Assets/js
+ERROR - 2024-05-10 05:26:05 --> 404 Page Not Found: Assets/js
+ERROR - 2024-05-10 05:26:05 --> 404 Page Not Found: Assets/js
+ERROR - 2024-05-10 05:26:05 --> 404 Page Not Found: Assets/js
+ERROR - 2024-05-10 05:26:05 --> 404 Page Not Found: Assets/js
+ERROR - 2024-05-10 05:26:05 --> 404 Page Not Found: Assets/js
+ERROR - 2024-05-10 05:26:05 --> 404 Page Not Found: Assets/js
+ERROR - 2024-05-10 05:26:05 --> 404 Page Not Found: Assets/js
+ERROR - 2024-05-10 05:26:16 --> 404 Page Not Found: Assets/js
+ERROR - 2024-05-10 05:26:16 --> 404 Page Not Found: Assets/js
+ERROR - 2024-05-10 05:26:16 --> 404 Page Not Found: Assets/js
+ERROR - 2024-05-10 05:26:16 --> 404 Page Not Found: Assets/js
+ERROR - 2024-05-10 05:26:16 --> 404 Page Not Found: Assets/js
+ERROR - 2024-05-10 05:26:16 --> 404 Page Not Found: Assets/js
+ERROR - 2024-05-10 05:26:16 --> 404 Page Not Found: Assets/js
+ERROR - 2024-05-10 05:26:16 --> 404 Page Not Found: Assets/js
+ERROR - 2024-05-10 05:27:00 --> 404 Page Not Found: Assets/js
+ERROR - 2024-05-10 05:27:00 --> 404 Page Not Found: Assets/js
+ERROR - 2024-05-10 05:27:00 --> 404 Page Not Found: Assets/js
+ERROR - 2024-05-10 05:27:00 --> 404 Page Not Found: Assets/js
+ERROR - 2024-05-10 05:27:00 --> 404 Page Not Found: Assets/js
+ERROR - 2024-05-10 05:27:00 --> 404 Page Not Found: Assets/js
+ERROR - 2024-05-10 05:27:00 --> 404 Page Not Found: Assets/js
+ERROR - 2024-05-10 05:27:00 --> 404 Page Not Found: Assets/js
+ERROR - 2024-05-10 05:49:29 --> Severity: Warning --> mysqli::real_connect(): (HY000/2002): No connection could be made because the target machine actively refused it.
+ C:\xampp74\htdocs\ekattor\system\database\drivers\mysqli\mysqli_driver.php 203
+ERROR - 2024-05-10 05:49:29 --> Unable to connect to the database
+ERROR - 2024-05-10 05:49:33 --> Severity: Warning --> mysqli::real_connect(): (HY000/2002): No connection could be made because the target machine actively refused it.
+ C:\xampp74\htdocs\ekattor\system\database\drivers\mysqli\mysqli_driver.php 203
+ERROR - 2024-05-10 05:49:33 --> Severity: Warning --> ini_set(): A session is active. You cannot change the session module's ini settings at this time C:\xampp74\htdocs\ekattor\system\libraries\Session\Session_driver.php 205
+ERROR - 2024-05-10 05:49:33 --> Severity: Warning --> session_start(): Failed to initialize storage module: user (path: C:\xampp74\tmp) C:\xampp74\htdocs\ekattor\system\libraries\Session\Session.php 143
